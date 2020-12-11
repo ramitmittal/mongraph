ecba976f8c55df0edca75c4fc9f1be49e0cd7b6f
Ramit Mittal <commitemailoframit@protonmail.com>
2020-12-12 00:49:32 +0530

    visual improvements in plot

--- mongraph/plot.py
+++ mongraph/plot.py
@@ -4,9 +4,8 @@ from bokeh.models import (
     Circle,
     HoverTool,
     MultiLine,
-    TapTool,
 )
-from bokeh.palettes import Spectral4
+from bokeh.palettes import Inferno6
 from bokeh.plotting import figure, from_networkx
 
 
@@ -41,14 +40,18 @@ def render(nodes):
         x_range=(-scale, scale),
         y_range=(-scale, scale),
     )
-    plot.add_tools(HoverTool(tooltips=attribute_list), TapTool())
+    plot.axis.visible = False
+
+    plot.add_tools(HoverTool(tooltips=attribute_list))
 
     graph = from_networkx(G, nx.spring_layout, scale=5, center=(0, 0))
 
-    graph.node_renderer.glyph = Circle(size=15, fill_color=Spectral4[0])
-    graph.node_renderer.hover_glyph = Circle(size=15, fill_color=Spectral4[1])
+    graph.node_renderer.glyph = Circle(size=15, fill_color=Inferno6[1], fill_alpha=0.6)
+    graph.node_renderer.hover_glyph = Circle(
+        size=15, fill_color=Inferno6[4], fill_alpha=0.6
+    )
     graph.edge_renderer.glyph = MultiLine(
-        line_color="#CCCCCC", line_alpha=0.8, line_width=5
+        line_color=Inferno6[0], line_alpha=0.3, line_width=3
     )
 
     plot.renderers.append(graph)
